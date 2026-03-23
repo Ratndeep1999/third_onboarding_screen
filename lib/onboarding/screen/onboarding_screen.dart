3cc442c5dc97fac07c0fcba696659e82e1822c76
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-23 21:02:37 +0530

    onboarding_screen.dart: Icon, Title and Description add.

--- lib/onboarding/screen/onboarding_screen.dart
+++ lib/onboarding/screen/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:third_onboarding_screen/utils/constants/app_texts.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -10,6 +11,51 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              /// Icon
+              Image(
+                alignment: Alignment.center,
+                image: AssetImage("assets/onboarding/onboarding_1.png"),
+                height: 300,
+                width: 300,
+                fit: BoxFit.contain,
+              ),
+              SizedBox(height: 30),
+
+              /// Title
+              Text(
+                AppText.kOnBoardingTitle1,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(height: 30),
+
+              /// Description
+              Text(
+                AppText.kOnBoardingDesc1,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w300,
+                  letterSpacing: 1,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
