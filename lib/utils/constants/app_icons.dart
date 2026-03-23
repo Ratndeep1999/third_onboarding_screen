916810b5412b26a0fc2ba1801c5d92f446eac07b
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-23 20:20:29 +0530

    app_icons.dart: AppIcon add.

---
+++ lib/utils/constants/app_icons.dart
@@ -0,0 +1,8 @@
+class AppIcons {
+  AppIcons._();
+
+  /// Onboarding icons
+  static const String kOnBoarding_icon1 = "assets/onboarding/onboarding_1.png";
+  static const String kOnBoarding_icon2 = "assets/onboarding/onboarding_2.png";
+  static const String kOnBoarding_icon3 = "assets/onboarding/onboarding_3.png";
+}
