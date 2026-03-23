17de4d5ea1ddf6515adcf1ef2d9c8a576d8d0cc4
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-23 20:37:27 +0530

    onboarding_data.dart: onboardingData variable add that holds list of OnboardingModel constructor with data.

---
+++ lib/onboarding/data/onboarding_data.dart
@@ -0,0 +1,21 @@
+import 'package:third_onboarding_screen/onboarding/model/onboarding_model.dart';
+import 'package:third_onboarding_screen/utils/constants/app_icons.dart';
+import 'package:third_onboarding_screen/utils/constants/app_texts.dart';
+
+List<OnboardingModel> onboardingData = [
+  OnboardingModel(
+    icon: AppIcons.kOnBoarding_icon1,
+    title: AppText.kOnBoardingTitle1,
+    desc: AppText.kOnBoardingDesc1,
+  ),
+  OnboardingModel(
+    icon: AppIcons.kOnBoarding_icon2,
+    title: AppText.kOnBoardingTitle2,
+    desc: AppText.kOnBoardingDesc2,
+  ),
+  OnboardingModel(
+    icon: AppIcons.kOnBoarding_icon3,
+    title: AppText.kOnBoardingTitle3,
+    desc: AppText.kOnBoardingDesc3,
+  ),
+];
