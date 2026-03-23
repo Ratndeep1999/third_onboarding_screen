a984cf445fe7fe3288e7e64ecef29e8849871945
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-23 20:34:51 +0530

    onboarding_model.dart: onboarding model add.

---
+++ lib/onboarding/model/onboarding_model.dart
@@ -0,0 +1,11 @@
+class OnboardingModel {
+  final String icon;
+  final String title;
+  final String desc;
+
+  OnboardingModel({
+    required this.icon,
+    required this.title,
+    required this.desc,
+  });
+}
