e991f3cddd4f17c6c11d4561a8a73c60cd020082
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-23 20:21:07 +0530

    app_texts.dart: onboarding titles and description add.

---
+++ lib/utils/constants/app_texts.dart
@@ -0,0 +1,16 @@
+class AppText {
+  AppText._();
+
+  /// OnBoarding Screens Title
+  static const String kOnBoardingTitle1 = 'On Boarding Title 1';
+  static const String kOnBoardingTitle2 = 'On Boarding Title 2';
+  static const String kOnBoardingTitle3 = 'On Boarding Title 3';
+
+  /// OnBoarding Screens Description
+  static const String kOnBoardingDesc1 =
+      "This is the description of Onboarding Screen 1";
+  static const String kOnBoardingDesc2 =
+      "This is the description of Onboarding Screen 2";
+  static const String kOnBoardingDesc3 =
+      "This is the description of Onboarding Screen 3";
+}
