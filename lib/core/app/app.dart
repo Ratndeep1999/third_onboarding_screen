2e2644f66c0a26fbe6cd004130a48bdd39cab9c0
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-23 20:00:06 +0530

    app.dart: MaterialApp() add.

---
+++ lib/core/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      home: OnboardingScreen(),
+    );
+  }
+}
