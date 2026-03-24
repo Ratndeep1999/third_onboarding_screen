6d4007294d6c45b9a9914d0db1b68f89e7c4542f
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-24 10:16:59 +0530

    shared_pref_service.dart: Single ton add.

---
+++ lib/core/services/shared_pref_service.dart
@@ -0,0 +1,8 @@
+class SharedPrefService {
+  /// Singleton
+  SharedPrefService._internal();
+
+  static final SharedPrefService _instance = SharedPrefService._internal();
+
+  static SharedPrefService get instance => _instance;
+}
