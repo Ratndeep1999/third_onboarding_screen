52ba947263317d2ca84a4bd5c53c68fe00b62034
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-24 10:49:55 +0530

    home_screen.dart: ElevatedButton() add that clear the onBoarding state.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:third_onboarding_screen/core/services/shared_pref_service.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Home Screen"), centerTitle: true),
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: SharedPrefService.instance.clearOnboardingState,
+            child: Text("Clear State"),
+          ),
+        ),
+      ),
+    );
   }
 }
