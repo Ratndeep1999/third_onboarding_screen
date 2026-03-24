db968793e9522db178817dd4955ccd4fc7403a95
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-24 10:23:26 +0530

    shared_pref_service.dart: static SharedPreferences? _prefs; add and initialize using getter _instancePref

--- lib/core/services/shared_pref_service.dart
+++ lib/core/services/shared_pref_service.dart
@@ -1,3 +1,5 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class SharedPrefService {
   /// Singleton
   SharedPrefService._internal();
@@ -5,4 +7,12 @@ class SharedPrefService {
   static final SharedPrefService _instance = SharedPrefService._internal();
 
   static SharedPrefService get instance => _instance;
+
+  /// Shared Preference Object
+  static SharedPreferences? _prefs;
+
+  /// Initialize Object
+  Future<SharedPreferences> get _instancePref async {
+    return _prefs ??= await SharedPreferences.getInstance();
+  }
 }
