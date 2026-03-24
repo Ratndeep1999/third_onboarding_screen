56d2ba09d38118416a8509458ce219b48436256a
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-24 10:58:45 +0530

    main.dart: WidgetsFlutterBinding.ensureInitialized(); add, final isSeen = await SharedPrefService.instance.getOnboardingStatus(); add and isSeen pass.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'core/app/app.dart';
+import 'core/services/shared_pref_service.dart';
 
-void main() {
-  runApp(const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Check onboarding status
+  final isSeen = await SharedPrefService.instance.getOnboardingStatus();
+  runApp(App(isSeen: isSeen));
 }
